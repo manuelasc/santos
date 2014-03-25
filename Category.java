6a84c9db63c7098f6b627c1183e5f93834e43d6f
Manuel Santos <cuatote1@gmail.com>
2014-03-25 13:01:21 -0600

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,10 @@
+package mx.edu.umg.personalkaban.model;
+public class Category {
+public String description;
+public String getDescription() {
+return description;	
+    }	
+public void setDescription(String description) {	
+this.description = description;	
+    }
+}
