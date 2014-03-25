ea9535acd47ec2ccb33efa0b4b7f9edf9b9b882c
Manuel Santos <cuatote1@gmail.com>
2014-03-25 13:34:11 -0600

    Create DashboardAbstract.java

---
+++ DashboardAbstract.java
@@ -0,0 +1,20 @@
+package mx.edu.umg.personalkaban.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class DashboardAbstract implements IDashboard {	
+
+protected List container;	
+
+public DashboardAbstract() {	
+    super();	
+    System.out.println("starting DashboardAbstract() ctor...");		
+    container = new ArrayList();	
+}	
+@Override	
+public boolean add(Task task) {	
+
+return container.add(task);	
+        }
+  }
